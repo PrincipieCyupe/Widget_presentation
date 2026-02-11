f1e9d5f71edb08560c23485cd41670f274e9c9f5
PrincipieCyupe <p.cyubahiro@alustudent.com>
2026-02-11 12:39:07 +0200

    Snackbar widget was mainly implemented well and can be pushed with no worries

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black54,
+          title: Text("Snack Bar"),
+        ),
+        body: SnackBarApp(),
+      ),
+    );
+  }
+}
+
+class SnackBarApp extends StatefulWidget {
+  const SnackBarApp({super.key});
+
+  @override
+  State<SnackBarApp> createState() => _SnackBarAppState();
+}
+
+class _SnackBarAppState extends State<SnackBarApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              child: ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text("This is a pop up message from the bottom"), backgroundColor: const Color.fromARGB(255, 102, 83, 22), duration: Duration(days: 1), action: SnackBarAction(label: "close", onPressed: (){}),),
+                  );
+                },
+                child: Text("Show Snack Bar"),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              },
+              child: Text("Hide Snack Bar"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
